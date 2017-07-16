906be2c3700b1e4cbf938fd82282b96e9e8b150b
DS\karodes <skarode96@gmail.com>
2017-07-16 12:29:56 +0530

    started with assignment 3

---
+++ Assignment_3/src/com/cdk/bank/Account/AccountService.java
@@ -0,0 +1,7 @@
+package com.cdk.bank.Account;
+
+/**
+ * Created by karodes on 7/15/2017.
+ */
+public class AccountService {
+}
---
+++ Assignment_3/src/com/cdk/bank/Customer/CustomerService.java
@@ -0,0 +1,7 @@
+package com.cdk.bank.Customer;
+
+/**
+ * Created by karodes on 7/15/2017.
+ */
+public class CustomerService {
+}
